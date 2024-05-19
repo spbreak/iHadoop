6032844d4f228544ee820cbb77a69f394e26df97
huanglexian <527108411@qq.com>
2024-05-19 13:30:42 +0800

    Maven创建

---
+++ 01-hadoopRpc/hadoop-common-project/hadoop-common/src/test/java/org/spbreak/hadoop/ipc/TestRPC.java
@@ -0,0 +1,11 @@
+package org.spbreak.hadoop.ipc;
+
+import org.junit.Test;
+
+public class TestRPC {
+    
+    @Test
+    public void testConnectionPing() throws Exception {
+        System.out.println("test");
+    }
+}
